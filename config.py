dc8526890567a285ce4643a1df0d559e82995414
root <root@ip-172-31-6-117.ap-southeast-1.compute.internal>
2016-11-15 11:14:34 +0000

    Minor issue fixed

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ class Auth:
     CLIENT_ID = ('563959952938-r23rkq28rs5mm3netmk9di3ne8kpr3f8.apps.googleusercontent.com')
     CLIENT_SECRET = 'KLn0sUmsXUkEfviIUsabmaAr'
     #REDIRECT_URI = 'http://lc-blr-292.ban.broadcom.com/gCallback'
+
     REDIRECT_URI = 'http://localhost:5000/gCallback'
+    REDIRECT_URI = 'http://ec2-54-255-134-178.ap-southeast-1.compute.amazonaws.com:5000/gCallback'
+
     AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
     TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
     USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
